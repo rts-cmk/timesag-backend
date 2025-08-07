14144cd27f448ea3ea49640c224c9c573c3ab91c
giaki <grigoriosvousios@gmail.com>
2025-08-07 13:56:49 +0200

    projects add to router.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import costumersRoutes from "./routes/costumers.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import usersRoutes from "./routes/users.routes.js";
+import projectsRoutes from "./routes/projects.routes.js";
 
 
 
@@ -11,6 +12,7 @@ const router = Router();
 usersRoutes(router);
 costumersRoutes(router);
 authRoutes(router);
+projectsRoutes(router);
 
 
 
