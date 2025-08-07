3349b3ed6f5bd5d0194f1cee8753ac4bd9fad9a8
giaki <grigoriosvousios@gmail.com>
2025-08-07 11:43:07 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -1,52 +1,31 @@
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import express from 'express'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import { authenticateToken } from './middleware.js'
+import router from './router.js'
+
+
+
 
 const prisma = new PrismaClient().$extends(withAccelerate())
 
 const app = express()
 app.use(express.json())
-
-app.get('/users', authenticateToken, async (req, res) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
-})
+app.use(router)
 
 
 
 
-app.post('/register', async (req, res) => {
-  const user = await prisma.user.create({
-    data: {
-      name: req.body.name,
-      email: req.body.email,
-      password: await bcrypt.hash(req.body.password, 10),
-    },
-  })
-  const { password, ...userwithoutpassword } = user
-  res.json(userwithoutpassword)
-})
 
-app.post('/login', async (req, res) => {
-  const user = await prisma.user.findUnique({
-    where: { email: req.body.email },
-  })
-  if (!user) {
-    return res.status(401).json({ message: 'Invalid email or password' })
-  }
-  const isValid = await bcrypt.compare(req.body.password, user.password)
-  if (!isValid) {
-    return res.status(401).json({ message: 'Invalid email or password' })
-  }
-  const { password, ...userwithoutpassword } = user
 
-  const token = jwt.sign({ userId: user.id, userName: user.name }, process.env.JWT_SECRET)
 
-  res.json({ ...userwithoutpassword, token })
-})
+
+
+
+
+
+
+
+
 
 app.listen(3000, () =>
   console.log(`
