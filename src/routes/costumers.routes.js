75b026c4b6e6cf9bde3663a5ff60ae894b2e0e0a
giaki <grigoriosvousios@gmail.com>
2025-08-11 09:31:48 +0200

    added post and delete

--- src/routes/costumers.routes.js
+++ src/routes/costumers.routes.js
@@ -16,4 +16,30 @@ export default function (router) {
         }
         res.json(customer)
     })
+
+
+    router.post('/costumers', authenticateToken, async (req, res) => {
+        const { company, email, phone, adress1, adress2, postalCode, city, contactperson, contactemail } = req.body
+        const costumer = await prisma.costumer.create({
+            data: {
+                company,
+                email,
+                phone,
+                adress1,
+                adress2,
+                postalCode,
+                city,
+                contactperson,
+                contactemail,
+            },
+        })
+        res.status(201).json(costumer)
+    })
+
+    router.delete('/costumers/:id', authenticateToken, async (req, res) => {
+        await prisma.costumer.delete({
+            where: { id: req.params.id },
+        })
+            return res.status(204).json({ message: 'costumer deleted' })
+    })
 }
