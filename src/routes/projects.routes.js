d21982541a728b2b9786c8c7b4ca8ed9296cd07a
giaki <grigoriosvousios@gmail.com>
2025-08-11 09:21:55 +0200

    added post and delete

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -8,14 +8,40 @@ export default function (router) {
     })
 
     router.get('/projects/:id', authenticateToken, async (req, res) => {
-        const project = await prisma.project.findUnique(
-            {
-                where: { id: req.params.id },
-            }
-        )
+        const project = await prisma.project.findUnique({
+            where: { id: req.params.id },
+        })
         if (!project) {
             return res.status(404).json({ message: 'project not found' })
         }
         res.json(project)
     })
+
+    router.post('/projects', authenticateToken, async (req, res) => {
+        const { name, description, customerId } = req.body
+        const project = await prisma.project.create({
+            data: {
+                name,
+                description,
+                customerId,
+            },
+        })
+        res.status(201).json(project)
+    })
+
+    
+    router.delete('/projects/:id', authenticateToken, async (req, res) => {
+        await prisma.project.delete({
+            where: { id: req.params.id },
+        })
+        return res.status(204).json({ message: 'project deleted' })
+    })
+
+
+
+
+
+
+
+
 }
