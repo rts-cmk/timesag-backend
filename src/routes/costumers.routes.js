c638eb2481884b9864c6ca809991f4029c1ed7bf
giaki <grigoriosvousios@gmail.com>
2025-08-11 13:05:23 +0200

    fixed typos

--- src/routes/costumers.routes.js
+++ src/routes/costumers.routes.js
@@ -12,34 +12,34 @@ export default function (router) {
             where: { id: req.params.id },
         })
         if (!customer) {
-            return res.status(404).json({ message: 'Customer not found' })
+            return res.status(404).json({ message: 'customer not found' })
         }
         res.json(customer)
     })
 
 
     router.post('/costumers', authenticateToken, async (req, res) => {
-        const { company, email, phone, adress1, adress2, postalCode, city, contactperson, contactemail } = req.body
-        const costumer = await prisma.costumer.create({
+        const { company, email, phone, address1, address2, postalCode, city, contactperson, contactemail } = req.body
+        const customer = await prisma.customer.create({
             data: {
                 company,
                 email,
                 phone,
-                adress1,
-                adress2,
+                address1,
+                address2,
                 postalCode,
                 city,
                 contactperson,
                 contactemail,
             },
         })
-        res.status(201).json(costumer)
+        res.status(201).json(customer)
     })
 
     router.delete('/costumers/:id', authenticateToken, async (req, res) => {
-        await prisma.costumer.delete({
+        await prisma.customer.delete({
             where: { id: req.params.id },
         })
-            return res.status(204).json({ message: 'costumer deleted' })
+            return res.status(204).json({ message: 'customer deleted' })
     })
 }
