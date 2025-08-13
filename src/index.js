5faf897ba257f3effcd5cf5d2308c762dde778d6
clurts <clu@rts.dk>
2025-08-13 09:32:51 +0200

    changed redirect in toml and added cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import router from './router.js'
 import prisma from './config/prismaClient.js'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use(router)
 
 app.listen(3000, () =>
