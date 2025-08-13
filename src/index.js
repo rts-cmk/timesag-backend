19e0f91e43cf05738f58b2edcb6b3524384403c0
clurts <clu@rts.dk>
2025-08-13 08:30:56 +0200

    remove cors

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@
 import express from 'express'
 import router from './router.js'
 import prisma from './config/prismaClient.js'
-import cors from 'cors'
 
 
 
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+
 
 
 app.use(router)
