27d33d8be40aa62ab27015d032d74dfddfc8048a
clurts <clu@rts.dk>
2025-08-13 08:30:26 +0200

    reverted prisma client

--- src/config/prismaClient.js
+++ src/config/prismaClient.js
@@ -1,7 +1,5 @@
-//import { withAccelerate } from '@prisma/extension-accelerate'
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient().$extends(withAccelerate())
 export default prisma
