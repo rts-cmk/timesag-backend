a2085343bbed086503dbbb1af366fece445f988e
clurts <clu@rts.dk>
2025-08-13 09:16:46 +0200

    more...

--- src/index.js
+++ src/index.js
@@ -1,32 +1,10 @@
-
 import express from 'express'
 import router from './router.js'
 import prisma from './config/prismaClient.js'
 
-
-
 const app = express()
-
 app.use(express.json())
-
-
-
 app.use(router)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, () =>
   console.log(`
